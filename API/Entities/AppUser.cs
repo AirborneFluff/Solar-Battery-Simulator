233623f0dd7445614a6fb8a0d93711260e01b69e
Fluff <proffitt.m.asidrain@gmail.com>
2022-07-02 16:05:35 +0100

    Added Geo data field to app user

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,10 @@ namespace API.Entities
 {
     public class AppUser : IdentityUser<int>
     {
+        public string? GeoBearerToken { get; set; }
+        public string? GeoDeviceId { get; set; }
+
+        public ICollection<VirtualBatterySystem> VirtualBatterySystems { get; set; } = new Collection<VirtualBatterySystem>();
         public ICollection<AppUserRole> UserRoles { get; set; } = new Collection<AppUserRole>();
     }
 }
